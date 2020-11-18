1f519e9bd2e14e4fb4ecfc3fcfced77d9435c738
Artem Golovin <artgolovin96@gmail.com>
2020-11-17 23:46:11 -0700

    clean up fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,3 @@
-# eval (starship init fish)
-
-# set -gx PATH (brew --prefix coreutils)/libexec/gnubin /usr/local/bin $PATH;
-
 set --export ANDROID $HOME/Library/Android
 set --export ANDROID_HOME $ANDROID/sdk
 set --export ANDROID_SDK $ANDROID_HOME
@@ -9,32 +5,18 @@ set --export GOPATH "$HOME/go"
 
 set -gx PATH $ANDROID_HOME/tools $PATH
 set -gx PATH $ANDROID_HOME/tools/bin $PATH
-# set -gx PATH $ANDROID_HOME/platform-tools $PATH;
 
 set --export PYENV_ROOT $HOME/.pyenv
 set -gx PATH $PYENV_ROOT/shims:$PYENV_ROOT/bin $PATH
 
-set --export PAYARA_HOME $HOME/payara5
-set --export GLASSFISH_HOME $HOME/payara5
-
-# set --export JAVA_HOME /Applications/Android\ Studio.app/Contents/jre/jdk/Contents/Home;
 set --export JAVA_HOME /Library/Java/JavaVirtualMachines/java-8.jdk/Contents/Home
 set -gx PATH $JAVA_HOME/bin $PATH
 
 set -gx PATH $HOME/.local/bin $PATH
-set -gx PATH $PAYARA_HOME/bin $PATH
 
 set -gx DYLD_LIBRARY_PATH "/usr/lib:/usr/local/lib" $DYLD_LIBRARY_PATH
 
 set -gx DOTS "$HOME/.dev/dotfiles"
-
-set -gx CFLAGS -I/usr/local/Cellar/argp-standalone/1.3/include/ $CFLAGS
-set -gx LDFLAGS -L/usr/local/Cellar/argp-standalone/1.3/lib/ -largp $LDFLAGS
-set -gx LDFLAGS "-L/usr/local/opt/bison/lib"
-set -gx LDFLAGS "-L/usr/local/opt/flex/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/flex/include"
-set -gx LDFLAGS "-L/usr/local/opt/openssl/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/openssl/include"
 
 # remove fish greeting
 set fish_greeting
@@ -64,17 +46,8 @@ alias vup="vagrant up"
 alias vhalt="vagrant halt"
 
 alias dots="cd $DOTS"
-# alias dl="youtube-dl -x --audio-format \"wav\" $1"
 
 funcsave vim
 funcsave ls
 funcsave cat
 funcsave python
-
-# thefuck --alias | source
-
-# status --is-interactive; and source (jenv init -|psub)
-
-set -g fish_user_paths "/usr/local/opt/bison/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/flex/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
